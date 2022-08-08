b037fbdf517800983181f094962bcb1e7bd294c1
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2022-08-08 23:01:03 +0100

    Delete GADS_PATH_DAAWK_MODULE_3_WORK_MANAGER directory

--- GADS_PATH_DAAWK_MODULE_3_WORK_MANAGER/app/src/main/java/com/example/gads_path_daawk_module_3_work_manager/WorkStatusSingleton.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gads_path_daawk_module_3_work_manager
-
-object WorkStatusSingleton {
-
-    var workComplete: Boolean = false
-
-    var workMessage: String = ""
-}
--- GADS_PATH_DAAWK_MODULE_3_WORK_MANAGER/app/src/main/java/com/example/gads_path_daawk_module_3_work_manager/part2/WorkStatusSingleton2.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gads_path_daawk_module_3_work_manager.part2
-
-object WorkStatusSingleton2 {
-
-    var workRetries: Int = 0
-}
